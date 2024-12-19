d487980c4b60c4d4f86cc9bb37fdd16b96df3810
kanha-13 <kanha.agr11@gmail.com>
2024-12-19 19:19:36 +0530

    added cursor pointer in header icons

--- src/components/template/Header.js
+++ src/components/template/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <div className="flex justify-between items-center h-[10%] w-full">
       <div className='flex lg:hidden'>
-        <div className='text-4xl -mt-2'>&equiv;</div>
+        <div className='cursor-pointer text-4xl -mt-2'>&equiv;</div>
       </div>
       <div className='flex bg-white px-5 items-center h-[60%] rounded-xl p-2 w-2/3 sm:w-3/4 md:w-[58%]'>
         <img className='mr-2' src={searchIcon} alt="search" />
@@ -17,13 +17,17 @@ const Header = () => {
       </div>
       <div className="w-1/4 sm:w-1/4 md:w-1/2 items-center flex justify-evenly">
         <div className='hidden md:flex items-center pl-6 justify-evenly w-[80%] xl:w-[55%]'>
-          <div><img src={helpIcon} alt="help" /></div>
-          <div className='relative'>
+          <div className='cursor-pointer relative'>
+            <img src={helpIcon} alt="help" />
+          </div>
+          <div className='cursor-pointer relative'>
             <img src={messagesIcon} alt="messages" />
             <span className="absolute -top-1.5 -right-2 block w-[11px] h-[11px] bg-red-500 rounded-full ring-2 ring-white"></span>
           </div>
-          <div><img src={settingstopIcon} alt="settings" /></div>
-          <div className='relative'>
+          <div className='cursor-pointer relative'>
+            <img src={settingstopIcon} alt="settings" />
+          </div>
+          <div className='cursor-pointer relative'>
             <span className="absolute -top-1.5 -right-1 block w-[11px] h-[11px] bg-red-500 rounded-full ring-2 ring-white"></span>
             <img src={notificationIcon} alt="notifications" />
           </div>
